2e852d4a04f23521ac0f10dc65933a67da8ce9e4
Sotiris Papatheodorou <sotirisp@protonmail.com>
2019-10-07 11:08:46 +0100

    Fix ROS keyserver

--- scripts/install_ros.sh
+++ scripts/install_ros.sh
@@ -6,8 +6,8 @@ install_ros() {
 	# http://wiki.ros.org/melodic/Installation/Ubuntu
 	# Setup repository
 	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-	sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-			--recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+	sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' \
+			--recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 	sudo apt update
 	# Install ros
 	sudo apt install ros-melodic-ros-base python-rosinstall python-catkin-tools
