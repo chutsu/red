89f454cbbb3e4a6ad75b0ac8e94db9814ce98421
Chris Choi <chutsu@gmail.com>
2019-11-29 23:40:47 +0000

    Remove redundant scripts

--- scripts/setup_drone.bash
+++ scripts/setup_drone.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e  # Exit on first error
 
-rm -rf $HOME/* # Clear all folders
+rm -rf "${HOME:?}"/* # Clear all folders
 sudo apt-get -qq install git make
 
 git clone https://github.com/chutsu/red
-cd red
-make install_deps
+cd red && make install_deps
